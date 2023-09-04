cbff048167f87852ae4ac9f83a6e5a11340a2953
Lexal1 <86933786+Lexal1@users.noreply.github.com>
2023-09-04 09:56:54 -0700

    made moon ores more rare hopefully

--- src/main/java/lexal/btb/world/generate/ChunkDecoratorMoon.java
+++ src/main/java/lexal/btb/world/generate/ChunkDecoratorMoon.java
@@ -30,21 +30,21 @@ public class ChunkDecoratorMoon implements ChunkDecorator {
         int xf;
         int yf;
         int zf;
-        for(max = 0; max < 25; ++max) {
+        for(max = 0; max < 10; ++max) {
             xf = x + rand.nextInt(16);
-            yf = minY + rand.nextInt(rangeY - 8) + 2;
+            yf = minY + rand.nextInt(rangeY - 12) + 2;
             zf = z + rand.nextInt(16);
             (new WorldFeatureMoonOre(ModBlocks.cheeseore.id, 4, false)).generate(this.world, rand, xf, yf, zf);
         }
-        for(max = 0; max < 50; ++max) {
+        for(max = 0; max < 25; ++max) {
             xf = x + rand.nextInt(16);
             yf = minY + rand.nextInt(rangeY - 8) + 4;
             zf = z + rand.nextInt(16);
             (new WorldFeatureMoonOre(ModBlocks.mooniron.id, 8, false)).generate(this.world, rand, xf, yf, zf);
         }
-        for(max = 0; max < 20; ++max) {
+        for(max = 0; max < 5; ++max) {
             xf = x + rand.nextInt(16);
-            yf = minY + rand.nextInt(rangeY - 8);
+            yf = minY + rand.nextInt(rangeY - 16);
             zf = z + rand.nextInt(16);
             (new WorldFeatureMoonOre(ModBlocks.moongold.id, 4, false)).generate(this.world, rand, xf, yf, zf);
         }
