49ae092a1a7d2d5d7670d0702528e38027c60a4e
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-09-08 06:05:30 -0500

    Added Delay Between Dimension Transfers
    Also fixed dimension scale being applied twice

--- src/main/java/lexal/btb/mixin/core/entity/EntityPlayerMixin.java
+++ src/main/java/lexal/btb/mixin/core/entity/EntityPlayerMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,6 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class EntityPlayerMixin extends EntityLiving {
     @Shadow
     public int dimension;
+    @Unique
+    private long timeOfLastDimensionShift = 0;
 
     public EntityPlayerMixin(World world) {
         super(world);
@@ -46,15 +49,18 @@ public class EntityPlayerMixin extends EntityLiving {
 
     @Inject(method = "tick()V", at = @At("TAIL"))
     private void moonThreshold(CallbackInfo ci){
-        if (this.y > this.world.getWorldType().getMaxY()+5 && !world.isDaytime()){
-
+        int delayMillis = 8000;
+        long currentTime = System.currentTimeMillis();
+        if (this.y > this.world.getWorldType().getMaxY()+5 && !world.isDaytime() && (currentTime-timeOfLastDimensionShift > delayMillis)){
             int targetDim = ModDimensions.dimensionMoon.id;
 
             if (this.dimension == targetDim) {
                 ModDimensions.dimensionShift(0);
             } else {
                 ModDimensions.dimensionShift(targetDim);
+                this.yd = 0;
             }
+            timeOfLastDimensionShift = currentTime;
         }
     }
 
