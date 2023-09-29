63e80b3f271f4d04b3b94057ccc4aa0260273296
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-09-28 23:21:15 -0500

    IInventory Implementation

--- src/main/java/lexal/btb/block/TileEntityInscriber.java
+++ src/main/java/lexal/btb/block/TileEntityInscriber.java
@@ -6,34 +6,50 @@ import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.player.inventory.IInventory;
 
 public class TileEntityInscriber extends TileEntity implements IInventory {
+    protected ItemStack[] inscriberItemStacks = new ItemStack[3];
     @Override
     public int getSizeInventory() {
-        return 0;
+        return inscriberItemStacks.length;
     }
 
     @Override
     public ItemStack getStackInSlot(int i) {
-        return null;
+        return inscriberItemStacks[i];
     }
 
     @Override
     public ItemStack decrStackSize(int i, int j) {
+        if (this.inscriberItemStacks[i] != null) {
+            if (this.inscriberItemStacks[i].stackSize <= j) {
+                ItemStack itemstack = this.inscriberItemStacks[i];
+                this.inscriberItemStacks[i] = null;
+                return itemstack;
+            }
+            ItemStack itemstack1 = this.inscriberItemStacks[i].splitStack(j);
+            if (this.inscriberItemStacks[i].stackSize <= 0) {
+                this.inscriberItemStacks[i] = null;
+            }
+            return itemstack1;
+        }
         return null;
     }
 
     @Override
     public void setInventorySlotContents(int i, ItemStack itemStack) {
-
+        this.inscriberItemStacks[i] = itemStack;
+        if (itemStack != null && itemStack.stackSize > this.getInventoryStackLimit()) {
+            itemStack.stackSize = this.getInventoryStackLimit();
+        }
     }
 
     @Override
     public String getInvName() {
-        return null;
+        return "Inscriber";
     }
 
     @Override
     public int getInventoryStackLimit() {
-        return 0;
+        return 64;
     }
 
     @Override
