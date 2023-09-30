55eb9ce82bdccc57e422f42729ee1c1e9d1662e7
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-09-29 19:19:03 -0500

    Days Achievement

--- src/main/java/lexal/btb/mixin/Entity/EntityPlayerMixin.java
+++ src/main/java/lexal/btb/mixin/Entity/EntityPlayerMixin.java
@@ -1,12 +1,25 @@
 package lexal.btb.mixin.Entity;
 
+import lexal.btb.ModAchievements;
 import lexal.btb.block.TileEntityInscriber;
 import lexal.btb.entity.IPlayerDisplay;
 import net.minecraft.core.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = EntityPlayer.class, remap = false)
 public class EntityPlayerMixin implements IPlayerDisplay {
+    @Unique
+    private EntityPlayer thisAs = (EntityPlayer)(Object)this;
+    @Inject(method = "tick()V", at = @At("TAIL"))
+    private void addAchievement(CallbackInfo ci){
+        if (thisAs.world.getWorldTime() / thisAs.world.worldType.getDayNightCycleLengthTicks() >= 100){
+            thisAs.triggerAchievement(ModAchievements.DAYS);
+        }
+    }
     @Override
     public void displayGUIInscriber(TileEntityInscriber tileEntityInscriber) {
 
